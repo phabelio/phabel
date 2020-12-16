ff30c57d989cdac07a814b826ce2fb849ffbefea
Daniil Gentili <daniil@daniil.it>
2020-12-16 21:25:44 +0100

    Remove leftovers

--- test/Target/Php71/ListExpressionTest.php
+++ test/Target/Php71/ListExpressionTest.php
@@ -24,25 +24,4 @@ class ListExpressionTest extends TestCase
         $this->assertEquals('d', $d);
         $this->assertEquals('e', $e);
     }
-    /**
-     * Parse list assignment.
-     *
-     * @param Assign $node List assignment
-     *
-     * @return ?Variable
-     */
-    public function enterAssign(Assign $node, Context $ctx): ?Variable
-    {
-        $isStmt = $ctx->parents[0]->getAttribute('currentNode') === 'stmts';
-        if (!($node->var instanceof List_ || $node->var instanceof Array_) || $isStmt) {
-            return null;
-        }
-        $list = $node->var;
-        $var = $ctx->getVariable();
-        $assignments = ListReference::splitList($list, $var);
-
-        // On newer versions of php, the list assignment expression returns the original array
-        $ctx->insertBefore($node, new Assign($var, $node->expr), ...$assignments);
-        return $var;
-    }
 }
