afe8e0229751dae2c66a7c19a31292749ed4078d
Daniil Gentili <daniil@daniil.it>
2021-01-17 17:14:32 +0100

    Use custom deploy key

--- tools/configGit.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$user = "41898282+github-actions[bot]@users.noreply.github.com";
-\passthru("git config --local user.email ".\escapeshellarg($user));
-\passthru("git config --local user.name ".\escapeshellarg("Github Actions"));
