e48ab04b91716908c5b31a62a154024aa5c813fe
Daniil Gentili <daniil@daniil.it>
2021-01-17 16:25:49 +0100

    Simplify

--- tools/configGit.php
+++ tools/configGit.php
@@ -1,9 +1,5 @@
 <?php
 
-$user = "github-actions[bot]";
-
-$res = \json_decode(\file_get_contents("https://api.github.com/users/$user"), true);
-$user = "${res['login']}+${res['id']}@users.noreply.github.com";
-
+$user = "41898282+github-actions[bot]@users.noreply.github.com";
 \passthru("git config --local user.email ".\escapeshellarg($user));
 \passthru("git config --local user.name ".\escapeshellarg("Github Actions"));
