fd2af904a073c8f138d21cf6117eae3584a79419
Daniil Gentili <daniil@daniil.it>
2021-03-26 17:19:24 +0100

    CI improvements

--- tools/ci/prepareDeps.sh
+++ tools/ci/prepareDeps.sh
@@ -5,10 +5,16 @@ composer install --prefer-dist --ignore-platform-reqs --no-plugins
 git stash
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-VERSION=${BRANCH: -3}
-git checkout ${BRANCH:0:-3}
+
+if [ $BRANCH != master ]; then
+    VERSION=${BRANCH: -3}
+    git checkout ${BRANCH:0:-3}
+fi
 
 php tools/ci/prepareDeps.php $VERSION
 
-git checkout $BRANCH
+if [ $BRANCH != master ]; then
+    git checkout $BRANCH
+fi
+
 git stash pop
