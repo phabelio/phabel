3fc826b0b3d88f3fb28ce6ca66ef4c52753266f4
Daniil Gentili <daniil@daniil.it>
2021-03-26 17:42:57 +0100

    Memory optimizations

--- src/Traverser.php
+++ src/Traverser.php
@@ -255,7 +255,7 @@ class Traverser
                             [$classes, $result] = $res;
                             if ($classStorage) {
                                 $classStorage->merge($classes);
-                            } else {
+                            } else if ($classes) {
                                 $classStorage = $classes;
                             }
                             unset($promises[$count]);
@@ -273,7 +273,7 @@ class Traverser
             yield $promises;
 
             if ($classStorage) {
-                yield self::runAsync($classStorage->finish(), $output, $output, $prefix);
+                self::run($classStorage->finish(), $output, $output, $prefix);
             }
 
             return $result;
