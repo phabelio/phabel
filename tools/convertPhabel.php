9bbcab79ae831da4ca26cc62636e4caaa0dadf35
Daniil Gentili <daniil@daniil.it>
2021-08-29 17:08:33 +0200

    Fix

--- tools/convertPhabel.php
+++ tools/convertPhabel.php
@@ -174,9 +174,9 @@ foreach ($target === 'all' ? Php::VERSIONS : [$target] as $realTarget) {
     r("rm -rf vendor-bin");
 
     if (!$dry) {
+        r("rm -rf vendor-bin");
         commit("phabel.io: add dependencies");
         \chdir("../phabelConvertedRepo");
-        r("rm -rf vendor-bin");
         if ($tag) {
             r("git tag ".\escapeshellarg("$tag.$target"));
             r("git push -f origin " . \escapeshellarg("$tag.$target"));
