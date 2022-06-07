0c89b80d69dda9cb08722065b3e159f4bc875e15
Daniil Gentili <daniil@daniil.it>
2022-06-07 17:22:14 +0200

    Fix build

--- tools/ci/convert.sh
+++ tools/ci/convert.sh
@@ -29,6 +29,7 @@ export PATH="$HOME/.local/php/$PHABEL_TARGET:$PATH"
 
 git clean -ffdx
 git reset --hard
+git fetch master-$(echo $PHABEL_TARGET | sed 's/\.//g')
 git checkout master-$(echo $PHABEL_TARGET | sed 's/\.//g')
 
 run
